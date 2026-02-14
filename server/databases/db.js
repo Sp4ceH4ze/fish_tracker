6fc4c30b4e67afe5421db690c10bd94efc576231
Sp4ceH4ze <spacehaze89@protonmail.com>
2026-02-14 16:26:02 +0100

    Added functionality to db.js for creating the db and adding the tables we need.

--- server/databases/db.js
+++ server/databases/db.js
@@ -1,11 +1,63 @@
 const sqlite3 = require('sqlite3').verbose();
+const { promisify } = require('util');
 
-const db = new sqlite3.Database('./databases/database.db', err => {
+const db = new sqlite3.Database('./database.db', err => {
   if (err) {
     console.error('Error connecting to database.', err.message);
   } else {
     console.log('Connected to SQLite database.');
+    createTables();
   }
 });
+
+const run = promisify(db.run.bind(db));
+const get = promisify(db.get.bind(db));
+const all = promisify(db.all.bind(db));
+
+async function createTables() {
+  await run(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+  )
+`);
+
+  console.log('users table created');
+
+  await run(`
+  CREATE TABLE IF NOT EXISTS sessions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date TEXT NOT NULL,
+    location TEXT NOT NULL,
+    NOTES TEXT
+  )
+`);
+
+  console.log('sessions table created');
+
+  await run(`
+  CREATE TABLE IF NOT EXISTS catch (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    session_id INTEGER NOT NULL,
+    species_id INTEGER NOT NULL,
+    length_cm INTEGER,
+    weight_kg INTEGER,
+    bait_used TEXT,
+    time_caught TEXT
+  )
+`);
+
+  console.log('catch table created');
+
+  await run(`
+  CREATE TABLE IF NOT EXISTS species (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    is_active TEXT
+  )
+`);
+
+  console.log('species table created');
+}
 
 module.exports = db;
